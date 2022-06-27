d6ccf5b5d083e73260ec7b42ee5fbef9e5cff10d
jad amine <jade1992amine@gmail.com>
2022-06-27 18:58:56 +0300

    Created React app & added components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <NavLink to={"/"}>
+          <h1>Workout Buddy</h1>
+        </NavLink>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
