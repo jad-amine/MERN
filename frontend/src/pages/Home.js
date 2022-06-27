88d92cc76143407abe58f4f318dbe71fafc6887b
jad amine <jade1992amine@gmail.com>
2022-06-27 19:40:03 +0300

    Added Workouts component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+
+// components
+
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
