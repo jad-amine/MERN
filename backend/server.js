a701af53a9811e5deae88cdb10598bedeae82b6e
jad amine <jade1992amine@gmail.com>
2022-06-27 17:12:04 +0300

    Added workout Model

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 // Express app
 const app = express();
@@ -9,6 +10,12 @@ app.use(express.json());
 // Routes
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("listening to port 4000");
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening to port 4000");
+    });
+  })
+  .catch((err) => console.log(err));
